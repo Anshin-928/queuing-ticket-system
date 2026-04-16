9e0726c4d6eb19df2f45bb9ed719eb34104b1dff
Anshin-928 <anshin16928@gmail.com>
2026-04-16 10:34:42 +0900

    feat: add Loading component for displaying loading state

---
+++ app/admin/loading.tsx
@@ -0,0 +1,10 @@
+import Box from '@mui/material/Box'
+import CircularProgress from '@mui/material/CircularProgress'
+
+export default function Loading() {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
+      <CircularProgress />
+    </Box>
+  )
+}
